4a2147b996331c43f3f81ff77f393099f7041c0a
Don-Stevenson <dsteven1@gmail.com>
2020-08-12 14:46:05 -0400

    add dictionary and calls to dictionary

--- dataTypes.py
+++ dataTypes.py
@@ -3,3 +3,22 @@
 x = [1 ,2 ,3]
 
 print(len(x))
+
+# making a tuple = an immutable list
+
+y = ("a", "b")
+print(y)
+
+# can assign multiple values to a tuple at once 
+yellow = (255, 255, 0)
+r, g, b = yellow
+print(r, g, b)
+
+print(yellow)
+
+# making a dictionary, like json in JS
+
+person = {"name": "Alice", "email": "alice@example.com"}
+
+print(person['name'])
+print(person)
